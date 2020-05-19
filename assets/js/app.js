227ab213476d004a4a3d92db72fcee1147f60fe5
Dallin Rodgers <rodgers.dallin@gmail.com>
2020-05-18 20:40:03 -0600

    Update app.js
    started js
    created click listener,
    add valication to make sure input is not empty strings
    started functions to create li

---
+++ assets/js/app.js
@@ -0,0 +1,70 @@
+var addButton = document.querySelector("#addButton");
+var newItemInput = document.querySelector("#new");
+var itemList = document.querySelector("#list");
+
+var newItem;
+var thingsToDo = [];
+
+addButton.addEventListener("click", () => {
+  newItem = newItemInput.value;
+  if (notEmptyStr(newItem)) {
+    console.log(newItem);
+    thingsToDo.push(newItem);
+    console.log(thingsToDo.length);
+    addToDo(thingsToDo.length - 1);
+  }
+});
+
+function notEmptyStr(str) {
+  if (str.trim().length > 0) {
+    // console.log("has something");
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function addToDo(here) {
+  console.log(here);
+  var newLi = document.createElement("li");
+
+  var item = createItem();
+  newLi.appendChild(item);
+
+  var action = createActions();
+  newLi.appendChild(action);
+
+  itemList.appendChild(newLi);
+
+  function createItem() {
+    var newItem = document.createElement("div");
+    newItem.className = "item";
+    var newToDo = document.createTextNode(thingsToDo[here]);
+    newItem.appendChild(newToDo);
+    return newItem;
+  }
+
+  function createActions() {
+    var newAction = document.createElement("div");
+    newAction.className = "actions";
+
+    var editSpan = createSpan("edit");
+
+    newAction.appendChild(editSpan);
+
+    return newAction;
+  }
+
+  function createSpan(what) {
+    var newSpan = document.createElement("span");
+    newSpan.classList.add(what, "icon");
+    newSpan.title = what;
+
+    var newIcon = document.createElement("i");
+    newIcon.classList.add("fas", "fa-cogs");
+
+    newSpan.appendChild(newIcon);
+
+    return newSpan;
+  }
+}
